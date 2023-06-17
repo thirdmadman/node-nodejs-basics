b0d4806bd576e9d13d3d13fa08d6634c5266a6a7
thirdmadman <thirdmadman@gmail.com>
2023-06-17 14:49:56 +0200

    feat: impl subtask rename.js

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,32 @@
-const rename = async () => {
-    // Write your code here 
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+/**
+ * brief rename.js
+ * Renames file wrongFilename.txt to properFilename with extension .md
+ *
+ * if there's no file wrongFilename.txt or properFilename.md already exists
+ * Error with message "FS operation failed" must be thrown
+ *
+ * @param {string} srcFileName
+ * @param {string} distFilename
+ */
+
+const rename = async (srcFileName = 'files/wrongFilename.txt', distFilename = 'files/properFilename.md') => {
+  const dirname = path.dirname(fileURLToPath(import.meta.url));
+  const srcFile = path.join(dirname, srcFileName);
+  const distFile = path.join(dirname, distFilename);
+
+  fs.access(srcFile, fs.F_OK, (accessError) => {
+    if (accessError) throw new Error('FS operation failed');
+  });
+
+  fs.access(distFile, fs.F_OK, (accessError) => {
+    if (!accessError) throw new Error('FS operation failed');
+  });
+
+  return fs.rename(srcFile, distFile, (err) => { if (err) throw err; });
 };
 
 await rename();
