b4d1c5b610a7ee4d5db8cac367d3088d292c15e9
thirdmadman <thirdmadman@gmail.com>
2023-06-17 15:54:07 +0200

    feat: impl subtask args.js

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,14 @@
+/**
+ * args.js
+ * Parses command line arguments
+ * (given in format --propName value --prop2Name value2 with no validation)
+ * and prints them to the console in the format propName is value, prop2Name is value2
+ */
+
 const parseArgs = () => {
-    // Write your code here 
+  const args = process.argv.slice(2);
+  const combinedArgs = args.join(',').split('--').slice(1);
+  console.log(combinedArgs.map((el) => el.split(',').slice(0, 2).join(' is ')).join(', '));
 };
 
 parseArgs();
