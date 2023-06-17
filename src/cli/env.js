798a3c9f913cbff1d7be089b4325a1abea1ce113
thirdmadman <thirdmadman@gmail.com>
2023-06-17 15:35:00 +0200

    feat: impl subtask env.js

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,14 @@
-const parseEnv = () => {
-    // Write your code here 
+/**
+ * env.js
+ * Parses environment variables with prefix RSS_ and prints them to the console
+ * in the format RSS_name1=value1; RSS_name2=value2
+ * @param {string} parsePrefix
+ */
+
+const parseEnv = (parsePrefix = 'RSS_') => {
+  const keys = Object.keys(process.env).filter((name) => name.indexOf(parsePrefix) === 0);
+  const strings = keys.map((key) => `${key}=${process.env[key]};`);
+  console.log(strings.join(' '));
 };
 
 parseEnv();
