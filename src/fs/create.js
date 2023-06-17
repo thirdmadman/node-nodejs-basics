5ce0e15daac69317dee18f0e7cd154fcf5dcd701
thirdmadman <thirdmadman@gmail.com>
2023-06-17 14:25:01 +0200

    feat: impl subtask create.js

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,18 @@
-const create = async () => {
-    // Write your code here 
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const create = async (data = 'I am fresh and young', fileRelativePath = 'files', fileName = 'fresh.txt') => {
+  const dirname = path.dirname(fileURLToPath(import.meta.url));
+  const filePath = path.join(dirname, fileRelativePath, fileName);
+
+  fs.access(filePath, fs.F_OK, (accessError) => {
+    if (!accessError) throw new Error('FS operation failed');
+
+    return fs.writeFile(filePath, data, { encoding: 'utf-8', flag: 'w' }, (writeError) => {
+      if (writeError) throw new Error(writeError);
+    });
+  });
 };
 
 await create();
