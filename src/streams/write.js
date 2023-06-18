fb08365b4a9bc380c44b939c471bfa3cdc2e6353
thirdmadman <thirdmadman@gmail.com>
2023-06-18 12:42:43 +0200

    feat: impl streams write.js

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,21 @@
-const write = async () => {
-    // Write your code here 
+import { createWriteStream } from 'fs';
+
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+/**
+ * write.js
+ * Writes process.stdin data into file fileToWrite.txt content using Writable Stream
+ * @param {string} relativePathToFile
+ */
+
+const write = async (relativePathToFile = 'files/fileToWrite.txt') => {
+  const dirname = path.dirname(fileURLToPath(import.meta.url));
+  const filePath = path.join(dirname, relativePathToFile);
+
+  const fileStream = createWriteStream(filePath, { flags: 'w' });
+
+  process.stdin.pipe(fileStream);
 };
 
 await write();
