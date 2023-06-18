2fcdb1d0aa49d0cde684b6d7719a14a3c5cdbe42
thirdmadman <thirdmadman@gmail.com>
2023-06-18 14:12:48 +0200

    feat: impl subtask compress.js

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,24 @@
-const compress = async () => {
-    // Write your code here 
+import { createGzip } from 'zlib';
+import { createWriteStream, createReadStream } from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+/**
+ * compress.js
+ * Compresses file fileToCompress.txt to archive.gz using zlib and Streams API
+ *
+ * @param {string} relativePathToFile
+ */
+
+const compress = async (relativePathToFile = 'files/fileToCompress.txt', relativeOutputFilePath = 'files/archive.gz') => {
+  const dirname = path.dirname(fileURLToPath(import.meta.url));
+  const filePath = path.join(dirname, relativePathToFile);
+  const outputFilePath = path.join(dirname, relativeOutputFilePath);
+
+  const compressOutputStream = createWriteStream(outputFilePath);
+  const inputFileStream = createReadStream(filePath);
+
+  inputFileStream.pipe(createGzip()).pipe(compressOutputStream);
 };
 
 await compress();
