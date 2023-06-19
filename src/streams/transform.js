86821a68e621d4e10755a1355441eef528ac5a42
thirdmadman <thirdmadman@gmail.com>
2023-06-20 00:08:15 +0200

    feat: impl subtask transform.js

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,20 @@
+import { Transform } from 'stream';
+
+/**
+ * transform.js
+ * Reads data from process.stdin, reverses text using Transform Stream and
+ * then writes it into process.stdout
+ */
+
 const transform = async () => {
-    // Write your code here 
+  const reverseTransform = new Transform({
+    transform(chunk, encoding, callback) {
+      const reversedChunk = chunk.toString().split('').reverse().join('');
+      callback(null, reversedChunk);
+    },
+  });
+
+  process.stdin.pipe(reverseTransform).pipe(process.stdout);
 };
 
 await transform();
